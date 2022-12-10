1aafeaf6684fa9235d24ebe9c42c0fa15230246c
huzhi <hu.zhi@outlook.com>
2022-12-11 00:28:53 +0800

    lab07 done

--- lab07/transpose.c
+++ lab07/transpose.c
@@ -1,9 +1,12 @@
 #include "transpose.h"
 
 /* The naive transpose function as a reference. */
-void transpose_naive(int n, int blocksize, int *dst, int *src) {
-    for (int x = 0; x < n; x++) {
-        for (int y = 0; y < n; y++) {
+void transpose_naive(int n, int blocksize, int *dst, int *src)
+{
+    for (int x = 0; x < n; x++)
+    {
+        for (int y = 0; y < n; y++)
+        {
             dst[y + x * n] = src[x + y * n];
         }
     }
@@ -11,6 +14,25 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
 
 /* Implement cache blocking below. You should NOT assume that n is a
  * multiple of the block size. */
-void transpose_blocking(int n, int blocksize, int *dst, int *src) {
+void transpose_blocking(int n, int blocksize, int *dst, int *src)
+{
     // YOUR CODE HERE
+    for (int x = 0; x < n; x = x + blocksize)
+    { // iterate using block size
+        for (int y = 0; y < n; y = y + blocksize)
+        { // iterate using block size
+            // Now begin transposition of each block
+            for (int i = x; i < x + blocksize; i++)
+            {
+                for (int j = y; j < y + blocksize; j++)
+                {
+                    if (i >= n || j >= n)
+                    {
+                        continue;
+                    }
+                    dst[j + i * n] = src[i + j * n];
+                }
+            }
+        }
+    }
 }
